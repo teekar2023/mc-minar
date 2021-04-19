4fbc55a7a8bdc8b03a7dcb5a60ff30655d9cf147
Sreekar Palla <67520739+teekar2023@users.noreply.github.com>
2021-04-19 14:55:29 -0400

    Add files via upload

--- minar.py
+++ minar.py
@@ -6,30 +6,30 @@ import sys
 
 def movar():
     print("movar started")
+    kbm.keyDown("shift")
+    interval = float(10)  # TODO do maths
     while True:
-        for i in range(12):
-            time.sleep(0.5)
-            kbm.keyDown("d")
-            time.sleep(0.03)
-            kbm.keyUp("d")
-        time.sleep(0.5)
+        kbm.keyDown("d")
+        time.sleep(interval)
+        kbm.keyUp("d")
         kbm.keyDown("s")
         time.sleep(0.05)
         kbm.keyUp("s")
-        for i in range(12):
-            time.sleep(0.5)
-            kbm.keyDown("a")
-            time.sleep(0.03)
-            kbm.keyUp("a")
-        time.sleep(0.5)
+        kbm.keyDown("a")
+        time.sleep(interval)
+        kbm.keyUp("a")
         kbm.keyDown("w")
-        time.sleep(0.02)
+        time.sleep(0.05)
         kbm.keyUp("w")
+        # TODO test
 
 
 def minar():
     print("minar starter")
     kbm.mouseDown()
+    time.sleep(num)
+    kbm.mouseUp()
+    kbm.keyUp("shift")
 
 
 open("minar_logs.log", "w+")
@@ -39,7 +39,7 @@ r = requests.get(url, allow_redirects=True)
 redirected_url = r.url
 if str(redirected_url) != "https://github.com/teekar2023/endstone-minar/releases/tag/v1.0":
     logging.warning("There Is New Update Available")
-    input("There Is An Update Available Check Discord And Download It Nigga...")
+    input(f"There Is An Update Available Check Discord And Download It Niggar...Or Use This Link: {str(redirected_url)}")
     exit()
 else:
     pass
